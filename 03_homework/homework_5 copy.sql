fbf3d659fd27f71242e3d5d12df52e0e717eaede
Ernani Fantinatti <ernanif@fantinatti.com>
2024-05-23 21:59:34 -0300

    homework_5 delivered!

---
+++ 03_homework/homework_5 copy.sql
@@ -0,0 +1,193 @@
+-- String manipulations
+/* 1. Some product names in the product table have descriptions like "Jar" or "Organic". 
+These are separated from the product name with a hyphen. 
+Create a column using SUBSTR (and a couple of other commands) that captures these, but is otherwise NULL. 
+Remove any trailing or leading whitespaces. Don't just use a case statement for each product! 
+
+| product_name               | description |
+|----------------------------|-------------|
+| Habanero Peppers - Organic | Organic     |
+
+Hint: you might need to use INSTR(product_name,'-') to find the hyphens. INSTR will help split the column. */
+--A:
+SELECT TRIM(SUBSTR(product_name,INSTR(product_name,'-') + 1)) AS description
+FROM product
+WHERE INSTR(product_name, '-') > 0;
+
+/* 2. Filter the query to show any product_size value that contain a number with REGEXP. */
+SELECT * FROM product
+WHERE product_size REGEXP('[0-9]')
+
+
+-- UNION
+/* 1. Using a UNION, write a query that displays the market dates with the highest and lowest total sales.
+
+HINT: There are a possibly a few ways to do this query, but if you're struggling, try the following: 
+1) Create a CTE/Temp Table to find sales values grouped dates; 
+2) Create another CTE/Temp table with a rank windowed function on the previous query to create 
+"best day" and "worst day"; 
+3) Query the second temp table twice, once for the best day, once for the worst day, 
+with a UNION binding them. */
+
+
+
+-- Cross Join
+/*1. Suppose every vendor in the `vendor_inventory` table had 5 of each of their products to sell to **every** 
+customer on record. How much money would each vendor make per product? 
+Show this by vendor_name and product name, rather than using the IDs.
+
+HINT: Be sure you select only relevant columns and rows. 
+Remember, CROSS JOIN will explode your table rows, so CROSS JOIN should likely be a subquery. 
+Think a bit about the row counts: how many distinct vendors, product names are there (x)?
+How many customers are there (y). 
+Before your final group by you should have the product of those two queries (x*y).  */
+--A:
+-- 1) Use CROSS JOIN to get all combinations of vendors, products, and customers
+WITH all_combinations AS (
+  SELECT 
+    v.vendor_name, p.product_name, c.customer_id, 5 * vi.price_per_unit AS revenue_per_customer
+  FROM vendor_inventory vi
+  CROSS JOIN customer c
+  JOIN vendor v ON vi.vendor_id = v.vendor_id
+  JOIN product p ON vi.product_id = p.product_id
+),
+
+-- 2) Calculate total revenue per vendor per product
+total_revenue AS (
+  SELECT
+    vendor_name,
+    product_name,
+    SUM(revenue_per_customer) AS total_revenue
+  FROM all_combinations
+  GROUP BY vendor_name, product_name
+)
+
+-- 3) Select relevant columns
+SELECT 
+  vendor_name,
+  product_name,
+  total_revenue
+FROM total_revenue
+ORDER BY vendor_name, product_name;
+
+
+
+-- INSERT
+/*1.  Create a new table "product_units". 
+This table will contain only products where the `product_qty_type = 'unit'`. 
+It should use all of the columns from the product table, as well as a new column for the `CURRENT_TIMESTAMP`.  
+Name the timestamp column `snapshot_timestamp`. */
+-- Class:
+
+DROP TABLE IF EXISTS temp.product_exoabded;
+CREATE TEMP TABLE product_exoabded AS
+	SELECT * FROM product;
+
+INSERT  INTO product_exoabded
+VALUES('26', 'Almonds', '1 lb',1, 'lbs')
+
+UPDATE product_exoabded
+SET product_size = '1/2 lb'
+WHERE product_id = 26;
+
+DELETE FROM product_exoabded
+WHERE product_id = 26
+
+SELECT * from product_exoabded
+WHERE product_id = 26
+
+/*2. Using `INSERT`, add a new row to the product_units table (with an updated timestamp). 
+This can be any product you desire (e.g. add another record for Apple Pie). */
+
+CREATE VIEW history AS
+
+
+-- DELETE
+/* 1. Delete the older record for the whatever product you added. 
+
+HINT: If you don't specify a WHERE clause, you are going to have a bad time.*/
+
+
+
+-- UPDATE
+/* 1.We want to add the current_quantity to the product_units table. 
+First, add a new column, current_quantity to the table using the following syntax.
+
+ALTER TABLE product_units
+ADD current_quantity INT;
+
+Then, using UPDATE, change the current_quantity equal to the last quantity value from the vendor_inventory details.
+
+HINT: This one is pretty hard. 
+First, determine how to get the "last" quantity per product. 
+Second, coalesce null values to 0 (if you don't have null values, figure out how to rearrange your query so you do.) 
+Third, SET current_quantity = (...your select statement...), remembering that WHERE can only accommodate one column. 
+Finally, make sure you have a WHERE statement to update the right row, 
+	you'll need to use product_units.product_id to refer to the correct row within the product_units table. 
+When you have all of these components, you can run the update statement. */
+
+
+--VIEW
+DROP VIEW IF EXISTS vendor_daily_sales;
+CREATE VIEW IF NOT EXISTS vendor_daily_sales AS
+	SELECT
+	md.market_date,
+	market_day,
+	market_year,
+	vendor_name,
+	SUM(quantity *cost_to_customer_per_qty) as sales
+	
+	FROM new_customer_purchases cp
+	INNER JOIN market_date_info md
+		ON cp.market_date = md.market_date
+	INNER JOIN vendor v
+		ON cp.vendor_id = v.vendor_id;
+		
+Select * from vendor_daily_sales;
+
+
+INSERT INTO market_date_info
+VALUES('2024-05-22','Wednesday','21','2024','8:00 AM', '2:00 PM', 'special day','Spring', '28','32',1,0)
+
+UPDATE new_customer_purchases
+SET market_date = '2024-05-22'
+
+SELECT * FROM new_customer_purchases
+SELECT * FROM market_date_info
+SELECT * FROM vendor_daily_sales
+
+SELECT * FROM new_customer_purchases
+UNION
+SELECT * FROM customer_purchases
+
+--TESTING CROSS JOIN
+DROP TABLE IF EXISTS temp.abc;
+CREATE TEMP TABLE IF NOT EXISTS temp.abc (col TEXT);
+INSERT INTO temp.abc
+VALUES('small');
+INSERT INTO temp.abc
+VALUES('medium');
+INSERT INTO temp.abc
+VALUES('large');
+
+SELECT * FROM abc
+
+-- Drop the second table if it exists
+DROP TABLE IF EXISTS temp.def;
+
+-- Create the second temporary table
+CREATE TEMP TABLE IF NOT EXISTS temp.def (col2 TEXT);
+
+-- Insert some values into the second table
+INSERT INTO temp.def VALUES('red');
+INSERT INTO temp.def VALUES('green');
+INSERT INTO temp.def VALUES('blue');
+
+SELECT * FROM def
+UNION
+SELECT * FROM abc
+
+-- Perform the cross join
+FROM temp.abc
+SELECT temp.abc.col, temp.def.col2
+CROSS JOIN temp.def;
